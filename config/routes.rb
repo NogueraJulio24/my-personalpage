6180969435293e11c5b7b75db428cabba6228a00
Julio Noguera <noguerajulio24@gmail.com>
2019-08-04 23:14:54 -0500

    error

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :educations
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  %w( 404 422 500 503 ).each do |code|
+    get code, :to => "errors#show", :code => code
+  end
 end
